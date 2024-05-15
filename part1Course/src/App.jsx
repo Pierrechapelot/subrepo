854c0d4d4049cc2ed218bf270e6efd25703f79bf
Pierrechapelot <pierre.chapelot@gmail.com>
2024-05-15 17:16:59 +0200

    part 1 course props

--- part1Course/src/App.jsx
+++ part1Course/src/App.jsx
@@ -1,11 +1,28 @@
-const App = () => {
-  const friends = [ 'Peter', 'Maya']
-
+const Hello = (props) => {
+  const bornYear = () => {
+    const yearNow= new Date().getFullYear()
+    return yearNow - props.age
+  }
   return (
     <div>
-      <p>{friends}</p>
+      <p>
+        Hello {props.name}, you are {props.age} years old
+      </p>
+      <p>So you were probably born in {bornYear()}</p>
     </div>
   )
 }
 
+const App = () => {
+  const name = 'Peter'
+  const age = 10
+
+  return (
+    <div>
+      <h1>Greetings</h1>
+      <Hello name="Maya" age={26 + 10} />
+      <Hello name={name} age={age} />
+    </div>
+  )
+}
 export default App
