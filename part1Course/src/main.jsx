09cfa42f28c56a753b876cf4305d349d7c9014cb
Pierrechapelot <pierre.chapelot@gmail.com>
2024-05-16 10:43:07 +0200

    refresh function

--- part1Course/src/main.jsx
+++ part1Course/src/main.jsx
@@ -3,4 +3,20 @@ import ReactDOM from 'react-dom/client'
 
 import App from './App'
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+let counter =1;
+console.log('counter1', counter)
+const refresh = () => {
+    ReactDOM.createRoot(document.getElementById('root')).render(
+    <App counter={counter} />
+)
+}
+
+refresh();
+counter += 1;
+console.log('counter2', counter)
+refresh();
+counter += 1;
+console.log('counter3', counter )
+refresh();
+
+
