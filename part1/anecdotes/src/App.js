8495a69d622dc0d622a4d5674282fceb416af2af
Pierrechapelot <pierre.chapelot@gmail.com>
2024-05-23 15:29:37 +0200

    axios

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -1,37 +1,28 @@
-import { useState } from 'react'
+import { useState } from "react";
 
 const Header = (props) => {
-  return (
-    <h1>{props.text}</h1>
-  )
-}
-
-const Button =({onClick, text}) => {
-  return (
-    <button onClick = {onClick}>{text}</button>
-  )
+  return <h1>{props.text}</h1>;
 };
 
-const Anecdote= ({text, voteCounts}) => {
+const Button = ({ onClick, text }) => {
+  return <button onClick={onClick}>{text}</button>;
+};
 
+const Anecdote = ({ text, voteCounts }) => {
   return (
     <div>
       <p>{text}</p>
       <p>has {voteCounts} votes</p>
-      </div>
+    </div>
+  );
+};
 
-  )
-}
-
-const BestAnecdote = ({anecdotes, allVotes}) => {
-
+const BestAnecdote = ({ anecdotes, allVotes }) => {
   const highestVoteCount = Math.max(...allVotes);
-  const bestAnecdoteIndex = allVotes.indexOf(highestVoteCount)
-  const bestAnecdote = anecdotes[bestAnecdoteIndex]
+  const bestAnecdoteIndex = allVotes.indexOf(highestVoteCount);
+  const bestAnecdote = anecdotes[bestAnecdoteIndex];
   if (highestVoteCount === 0) {
-    return (
-      <p>No votes yet</p>
-    )
+    return <p>No votes yet</p>;
   }
 
   return (
@@ -39,43 +30,43 @@ const BestAnecdote = ({anecdotes, allVotes}) => {
       <p>{bestAnecdote}</p>
       <p>has {highestVoteCount} votes</p>
     </div>
-  )
-}
+  );
+};
 
 const App = () => {
   const anecdotes = [
-    'If it hurts, do it more often.',
-    'Adding manpower to a late software project makes it later!',
-    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-    'Premature optimization is the root of all evil.',
-    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
-    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.'
-  ]
-   
-  const [selected, setSelected] = useState(0)
-  const [allVotes, setAllVotes]= useState(Array(7).fill(0));
+    "If it hurts, do it more often.",
+    "Adding manpower to a late software project makes it later!",
+    "The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
+    "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+    "Premature optimization is the root of all evil.",
+    "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
+    "Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.",
+  ];
 
+  const [selected, setSelected] = useState(0);
+  const [allVotes, setAllVotes] = useState(Array(7).fill(0));
 
-  const handleVoteClick =() =>{
+  const handleVoteClick = () => {
     const newAllVotes = [...allVotes];
-    newAllVotes[selected] +=1;
-    setAllVotes(newAllVotes)
-    };
+    newAllVotes[selected] += 1;
+    setAllVotes(newAllVotes);
+  };
 
-  const handleNextClick = () => setSelected(Math.floor(Math.random()*anecdotes.length))
+  const handleNextClick = () =>
+    setSelected(Math.floor(Math.random() * anecdotes.length));
 
   return (
     <div>
-    <Header text = 'Anecdote of the day'/>
-    <Anecdote text ={anecdotes[selected]} voteCounts={allVotes[selected]}/>
-    <Button onClick={handleVoteClick} text='Vote'/>
-    <Button onClick={handleNextClick} text='Next Anecdote'/>
-    
-    <Header text= 'Anecdote with most votes'/>
-    <BestAnecdote anecdotes={anecdotes} allVotes={allVotes}/>
-    </div>
-  )
-}
+      <Header text="Anecdote of the day" />
+      <Anecdote text={anecdotes[selected]} voteCounts={allVotes[selected]} />
+      <Button onClick={handleVoteClick} text="Vote" />
+      <Button onClick={handleNextClick} text="Next Anecdote" />
 
-export default App
+      <Header text="Anecdote with most votes" />
+      <BestAnecdote anecdotes={anecdotes} allVotes={allVotes} />
+    </div>
+  );
+};
+
+export default App;
--- part2/part2course/src/App.js
+++ part2/part2course/src/App.js
@@ -1,59 +1,52 @@
-import {useState} from 'react'
-import Note from './components/Note'
+import { useState } from "react";
+import Note from "./components/Note";
 
 const App = (props) => {
-  
-  const [notes, setNotes] = useState(props.notes)
-  const [newNote, setNewNote] = useState('a new note...')
-  const [showAll, setShowAll] = useState(true)
+  const [notes, setNotes] = useState(props.notes);
+  const [newNote, setNewNote] = useState("a new note...");
+  const [showAll, setShowAll] = useState(true);
 
   const addNote = (event) => {
-    event.preventDefault()
-    console.log('button clicked', event.target)
+    event.preventDefault();
+    console.log("button clicked", event.target);
     const noteObject = {
-      content : newNote,
+      content: newNote,
       date: new Date().toISOString(),
       important: Math.random() < 0.5,
-      id: notes.length +1
-    }
+      id: notes.length + 1,
+    };
     setNotes(notes.concat(noteObject));
-    console.log(notes)
-
-
-  }
+    console.log(notes);
+  };
 
   const handleNoteChange = (event) => {
-    console.log(event.target.value)
-    setNewNote(event.target.value)
+    console.log(event.target.value);
+    setNewNote(event.target.value);
+  };
 
-  }
-
-  const notesToShow = showAll ? notes : notes.filter(note => note.important === true)
-
-
+  const notesToShow = showAll
+    ? notes
+    : notes.filter((note) => note.important === true);
 
   return (
     <div>
       <h1>Notes</h1>
-      <div> 
+      <div>
         <button onClick={() => setShowAll(!showAll)}>
-          show {showAll ? 'important' : 'all'}
+          show {showAll ? "important" : "all"}
         </button>
-
-
       </div>
       <ul>
-        {notesToShow.map(note =>
-        <Note key={note.id} note ={note}/>
-
-          )}
+        {notesToShow.map((note) => (
+          <Note key={note.id} note={note} />
+        ))}
       </ul>
       <form onSubmit={addNote}>
-        <input value ={newNote} onChange={handleNoteChange} />
-        <button type='submit'>save</button>
+        <input value={newNote} onChange={handleNoteChange} />
+        <button type="submit">save</button>
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
