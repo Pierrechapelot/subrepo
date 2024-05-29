441f546340a9892756da56c2fceea2836f24cf4a
Pierrechapelot <pierre.chapelot@gmail.com>
2024-05-29 17:27:11 +0200

    phonebookBackend/step6

--- part3/phonebookBackend/index.js
+++ part3/phonebookBackend/index.js
@@ -49,10 +49,10 @@ app.get("/info", (request, response) => {
 
 app.get("/api/persons/:id", (request, response) => {
   const id = Number(request.params.id);
-//   console.log(id)
+  //   console.log(id)
   const person = persons.find((person) => person.id === id);
 
-//   console.log(person.id, id, typeof person.id, typeof id)
+  //   console.log(person.id, id, typeof person.id, typeof id)
   if (person) {
     response.json(person);
   } else {
@@ -60,39 +60,42 @@ app.get("/api/persons/:id", (request, response) => {
   }
 });
 
-app.delete("/api/persons/:id", (request, response)=> {
-    const id = Number(request.params.id);
-    persons = persons.filter((person) => person.id !== id);
-    response.status(204).end();
-})
+app.delete("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+  persons = persons.filter((person) => person.id !== id);
+  response.status(204).end();
+});
 
 const generateId = () => {
-    const randomId = Math.floor(Math.random() * 4000)
-    return randomId
-
-}
+  const randomId = Math.floor(Math.random() * 4000);
+  return randomId;
+};
 
 app.post("/api/persons", (request, response) => {
-    const body = request.body;
+  const body = request.body;
+  const existingName = persons.find((person) => person.name === body.name);
+  if (!body.name) {
+    return response.status(400).json({
+      error: "Name is missing",
+    });
+  } else if (!body.number) {
+    return response.status(400).json({
+      error: "Number is missing",
+    });
+  } else if (existingName) {
+    return response.status(400).json({
+      error: "Name must be unique",
+    });
+  }
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: generateId(),
+  };
+  persons = persons.concat(person);
 
-    if (!body.name) {
-        return response.status(400).json({
-            error: "Name is missing"
-        })
-        } else if (!body.number) {
-            return response.status(400).json({
-                error: "Number is missing"
-            })
-        }
-        const person = {
-            name: body.name,
-            number: body.number,
-            id: generateId(),
-        }
-        persons = persons.concat(person)
-
-        response.json(person)
-})
+  response.json(person);
+});
 
 const PORT = 3001;
 app.listen(PORT, () => {
