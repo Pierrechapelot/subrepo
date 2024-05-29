8688ae741dbb380f2c7401340a59521ea7643fd3
Pierrechapelot <pierre.chapelot@gmail.com>
2024-05-29 16:23:19 +0200

    phonebookBackend/steps1&2 done

---
+++ part3/phonebookBackend/index.js
@@ -0,0 +1,53 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+let persons = [
+  {
+    id: 1,
+    name: "Arto Hellas",
+    number: "040-123456",
+  },
+  {
+    id: 2,
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+  },
+  {
+    id: 3,
+    name: "Dan Abramov",
+    number: "12-43-234345",
+  },
+  {
+    id: 4,
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+  },
+];
+
+app.get("/api/persons", (request, response) => {
+  response.json(persons);
+});
+
+app.get("/info", (request, response) => {
+  const entries = persons.length;
+  const currentTime = new Date().toLocaleString();
+  const timeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+
+  response.send(
+    `
+    <div>
+      
+      <p>Phonebook has info for ${entries} people. </p>
+      <p>
+        ${currentTime} (${timeZone})
+      </p>
+    </div>`
+  );
+});
+
+const PORT = 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
