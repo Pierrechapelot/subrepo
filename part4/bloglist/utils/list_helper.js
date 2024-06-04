5da9c8c3d390f288d479b282747c998fdbee69f9
Pierrechapelot <pierre.chapelot@gmail.com>
2024-06-04 14:40:40 +0200

    totalLikes & favoriteBlog

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -1,7 +1,21 @@
 const dummy = (blogs) => {
-    return 1
-  }
-  
-  module.exports = {
-    dummy
-  }
+  return 1;
+};
+
+const totalLikes = (blogs) => {
+  return blogs.length === 0
+    ? 0
+    : blogs.reduce((sum, blog) => sum + blog.likes, 0);
+};
+
+const favoriteBlog = (blogs) => {
+    return blogs.length === 0
+    ? {}
+    : blogs.reduce((maxLikes, blog) => blog.likes > maxLikes ? blog.likes : maxLikes, blogs[0].likes)
+}
+
+
+module.exports = {
+  dummy,
+  totalLikes, favoriteBlog,
+};
