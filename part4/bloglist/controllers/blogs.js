e76bcc60bc1c02fa07e889569c21c77a9eefa6c0
Pierrechapelot <pierre.chapelot@gmail.com>
2024-06-05 14:55:19 +0200

    delete and put routes async await

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -3,12 +3,11 @@ const Blog = require("../models/blog");
 
 blogsRouter.get("/", async (request, response) => {
   const blogs = await Blog.find({});
-  response.json(blogs.map(blog => blog.toJSON()))
-
+  response.json(blogs.map((blog) => blog.toJSON()));
 });
 
 blogsRouter.post("/", async (request, response) => {
-  const body = request.body
+  const body = request.body;
   const blog = new Blog({
     title: body.title,
     author: body.author,
@@ -16,8 +15,29 @@ blogsRouter.post("/", async (request, response) => {
     likes: body.likes,
   });
 
-  const savedBlog = await blog.save().
-    response.status(201).json(savedBlog);
+  const savedBlog = await blog.save().response.status(201).json(savedBlog);
+});
+
+blogsRouter.delete("/:id", async (request, response, next) => {
+  const blogToDelete = await Blog.findById(request.params.id);
+  response.status(204).end();
+});
+
+blogsRouter.put("/:id", async (request, response, next) => {
+  const body = request.body;
+
+  const blog = {
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes,
+    comments: body.comments,
+  };
+
+  const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, {
+    new: true,
+  });
+  response.json(updatedBlog)
 });
 
 module.exports = blogsRouter;
