e65b66bfda686b9fe129b62e3d5c89c9a2293a3f
Pierrechapelot <pierre.chapelot@gmail.com>
2024-06-06 17:35:32 +0200

    reducer

--- part6/course/src/App.jsx
+++ part6/course/src/App.jsx
@@ -1,47 +1,43 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-import './App.css'
-import {createStore} from 'redux' 
-
-// const noteReducer = (state = [], action) => {
-//   if (action.type === 'NEW_NOTE') {
-//     return state.concat(action.payload)
-//   }
-//   return state
-// }
-
-const store = createStore(noteReducer)
-
-store.dispatch({
-  type: 'NEW_NOTE',
-  payload: {
-    content: 'the app state is in redux store',
-    important: true,
-    id: 1
-  }
-})
-
-store.dispatch({
-  type: 'NEW_NOTE',
-  payload: {
-    content: 'state changes are made with actions',
-    important: false,
-    id: 2
-  }
-})
+import { createNote, toggleImportanceOf } from './reducers/noteReducer'
+import { useSelector, useDispatch } from 'react-redux'
 
 const App = () => {
-  return(
+
+  const dispatch = useDispatch()
+  const notes = useSelector(state => state)
+
+  const addNote = (event) => {
+    event.preventDefault()
+    const content = event.target.note.value
+    event.target.note.value = ''
+
+    dispatch(createNote(content))
+  }
+
+  const toggleImportance = (id) => {
+
+    dispatch(toggleImportanceOf(id))
+  }
+
+  return (
     <div>
+      <form onSubmit={addNote}>
+        <input name="note" /> 
+        <button type="submit">add</button>
+      </form>
       <ul>
-        {store.getState().map(note=>
-          <li key={note.id}>
+
+        {notes.map(note =>
+          <li
+            key={note.id} 
+            onClick={() => toggleImportance(note.id)}
+          >
             {note.content} <strong>{note.important ? 'important' : ''}</strong>
           </li>
         )}
-        </ul>
+      </ul>
     </div>
   )
 }
+
 export default App
