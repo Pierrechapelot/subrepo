f0145c354f14556f7df79ffac493f3018219c460
Pierrechapelot <pierre.chapelot@gmail.com>
2024-06-10 08:34:40 +0200

    5.1 exercice

--- part5/bloglist-frontend/vite.config.js
+++ part5/bloglist-frontend/vite.config.js
@@ -6,10 +6,10 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': {
-        target: 'http://localhost:5173',
+      "/api": {
+        target: "http://localhost:3003",
         changeOrigin: true,
       },
-    }
+    },
   },
 })
