8470b9fdacf142de5b5ae488a03bb01610d524e8
Pierrechapelot <pierre.chapelot@gmail.com>
2024-06-10 17:27:25 +0200

    togglable tests

--- part5/notes/src/components/Note.test.jsx
+++ part5/notes/src/components/Note.test.jsx
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Note from './Note'
 
 test('renders content', () => {
@@ -7,9 +8,21 @@ test('renders content', () => {
     important: true
   }
 
-  render(<Note note={note} />)
+  const mockHandler = vi.fn()
+  render(
+    <Note note={note} toggleImportance={mockHandler} />
+  )
 
-  const element = screen.getByText('Component testing is done with react-testing-library')
-  screen.debug(element)
-  expect(element).toBeDefined()
+  const user = userEvent.setup()
+  const button = screen.getByText('make not important')
+  await user.click(button)
+
+  expect(mockHandler.mock.calls).toHaveLength(1)
 })
+
+//   render(<Note note={note} />)
+
+//   const element = screen.getByText('Component testing is done with react-testing-library')
+//   screen.debug(element)
+//   expect(element).toBeDefined()
+// })
