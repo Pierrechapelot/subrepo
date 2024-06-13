4895a66771b2ee6ab53e4f3375e2dd411555928d
Pierrechapelot <pierre.chapelot@gmail.com>
2024-06-13 15:35:17 +0200

    anecdotes/steps1-2-4-5-6

--- part6/redux-anecdotes/src/App.jsx
+++ part6/redux-anecdotes/src/App.jsx
@@ -1,34 +1,54 @@
-import { useSelector, useDispatch } from 'react-redux'
+import Anecdotes from "./components/Anecdotes"
+import NewAnecdote from "./components/NewAnecdote"
+
+
 
 const App = () => {
-  const anecdotes = useSelector(state => state)
-  const dispatch = useDispatch()
-
-  const vote = (id) => {
-    console.log('vote', id)
-  }
-
-  return (
-    <div>
-      <h2>Anecdotes</h2>
-      {anecdotes.map(anecdote =>
-        <div key={anecdote.id}>
-          <div>
-            {anecdote.content}
-          </div>
-          <div>
-            has {anecdote.votes}
-            <button onClick={() => vote(anecdote.id)}>vote</button>
-          </div>
-        </div>
-      )}
-      <h2>create new</h2>
-      <form>
-        <div><input /></div>
-        <button>create</button>
-      </form>
-    </div>
-  )
+return (
+  <div>
+       <h2>Anecdotes</h2>
+       <Anecdotes/>
+       <h2>Create new</h2>
+       <NewAnecdote/>
+</div>
+)
 }
+
+
+
+
+
+
+
+
+{/* {/* //   const anecdotes = useSelector(state => state)
+//   const dispatch = useDispatch()
+
+//   const vote = (id) => { */}
+{/* //     console.log('vote', id)
+//   }
+
+//   return (
+//     <div>
+//       <h2>Anecdotes</h2>
+//       {anecdotes.map(anecdote => */}
+{/* //         <div key={anecdote.id}>
+//           <div>
+//             {anecdote.content}
+//           </div>
+//           <div>
+//             has {anecdote.votes}
+//             <button onClick={() => vote(anecdote.id)}>vote</button>
+//           </div>
+//         </div>
+//       )}
+//       <h2>create new</h2>
+//       <form>
+//         <div><input /></div>
+//         <button>create</button>
+//       </form>
+//     </div> */}
+//   
+
 
 export default App
