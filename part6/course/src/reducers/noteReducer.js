456dd504d360b37adc6e66ed543a9222ecd9eb84
Pierrechapelot <pierre.chapelot@gmail.com>
2024-06-18 14:55:49 +0200

    redux thunk notes

--- part6/course/src/reducers/noteReducer.js
+++ part6/course/src/reducers/noteReducer.js
@@ -1,94 +1,43 @@
 import { createSlice } from "@reduxjs/toolkit";
-
-const initialState = [
-  {
-    content: "reducer defines how redux store works",
-    important: true,
-    id: 1,
-  },
-  {
-    content: "state of store can contain any data",
-    important: false,
-    id: 2,
-  },
-];
-
-const generateId = () => Number((Math.random() * 1000000).toFixed(0));
+import noteService from "../services/notes";
 
 const noteSlice = createSlice({
   name: "notes",
   initialState: [],
   reducers: {
-    createNote(state, action) {
+    toggleImportanceOf(state, action) {
+      const id = action.payload;
+      const noteToChange = state.find((n) => n.id === id);
+      const changedNote = {
+        ...noteToChange,
+        important: !noteToChange.important,
+      };
+      console.log(JSON.parse(JSON.stringify(state)));
+      return state.map((note) => (note.id !== id ? note : changedNote));
+    },
+    appendNote(state, action) {
       state.push(action.payload);
     },
-    // const content = action.payload
-    // state.push({
-    //   content,
-    //   important: false,
-    //   id: generateId(),
-    // })
-  },
-  toggleImportanceOf(state, action) {
-    const id = action.payload;
-    const noteToChange = state.find((n) => n.id === id);
-    const changedNote = {
-      ...noteToChange,
-      important: !noteToChange.important,
-    };
-    console.log(JSON.parse(JSON.stringify(state)));
-    return state.map((note) => (note.id !== id ? note : changedNote));
-  },
-  appendNote(state, action) {
-    state.push(action.payload);
-  },
-  setNotes(state, action) {
-    return action.payload;
+    setNotes(state, action) {
+      return action.payload;
+    },
   },
 });
 
-export const { createNote, toggleImportanceOf, appendNote, setNotes } =
-  noteSlice.actions;
+export const { toggleImportanceOf, appendNote, setNotes } = noteSlice.actions;
+
+export const initializeNotes = () => {
+  return async (dispatch) => {
+    const notes = await noteService.getAll();
+    dispatch(setNotes(notes));
+  };
+};
+
+export const createNote = (content) => {
+  return async (dispatch) => {
+    const newNote = await noteService.createNew(content);
+    dispatch(appendNote(newNote));
+  };
+};
+
 export default noteSlice.reducer;
-
-// const noteReducer = (state = initialState, action) => {
-//     switch(action.type) {
-//       case 'NEW_NOTE':
-//         return [...state, action.payload]
-//       case 'TOGGLE_IMPORTANCE': {
-//         const id = action.payload.id
-//         const noteToChange = state.find(n => n.id === id)
-//         const changedNote = {
-//           ...noteToChange,
-//           important: !noteToChange.important
-//         }
-//         return state.map(note =>
-//           note.id !== id ? note : changedNote
-//         )
-//        }
-//       default:
-//         return state
-//     }
-//   }
-//   const generateId = () =>
-//     Number((Math.random() * 1000000).toFixed(0))
-
-//   export const createNote = (content) => {
-//     return {
-//       type: 'NEW_NOTE',
-//       payload: {
-//         content,
-//         important: false,
-//         id: generateId()
-//       }
-//     }
-//   }
-
-//   export const toggleImportanceOf = (id) => {
-//     return {
-//       type: 'TOGGLE_IMPORTANCE',
-//       payload: { id }
-//     }
-//   }
-
-// export default noteReducer
