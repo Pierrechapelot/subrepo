3ee813ff34dbc570b47e78928949227a33558973
Pierrechapelot <pierre.chapelot@gmail.com>
2024-07-08 16:05:31 +0200

    add dev to project

--- part9/course/index.ts
+++ part9/course/index.ts
@@ -1,7 +1,8 @@
-import express from "express";
+import express from 'express';
+
 const app = express();
 
-app.get("/ping", (req, res) => {
+app.get("/ping", (_req, res) => {
   res.send("pong");
 });
 
