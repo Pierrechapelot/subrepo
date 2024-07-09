c94805ffc602e1f8b5672a77918bd57ec8f54ca7
Pierrechapelot <pierre.chapelot@gmail.com>
2024-07-09 09:35:41 +0200

    type assertion course

--- part9/course/multiplier.ts
+++ part9/course/multiplier.ts
@@ -1,18 +1,9 @@
-// const multiplicator = (a: number, b: number, printText: string) => {
-//     console.log(printText,  a * b);
-//   }
-
-//   const a: number = Number(process.argv[2])
-//   const b: number = Number(process.argv[3])
-  
-//   multiplicator(a, b, `Mulitplied ${a} and ${b}, the result is: `);
-
-  interface MultiplyValues {
+interface MultiplyValues {
     value1: number;
     value2: number;
   }
   
-  const parseArguments = (args: string[]): MultiplyValues => {
+  export const parseArguments = (args: string[]): MultiplyValues => {
     if (args.length < 4) throw new Error('Not enough arguments');
     if (args.length > 4) throw new Error('Too many arguments');
   
@@ -20,21 +11,21 @@
       return {
         value1: Number(args[2]),
         value2: Number(args[3])
-      }
+      };
     } else {
       throw new Error('Provided values were not numbers!');
     }
-  }
+  };
   
-  const multiplicator = (a: number, b: number, printText: string) => {
+  export const multiplicator = (a: number, b: number, printText: string) => {
     console.log(printText,  a * b);
-  }
+  };
   
   try {
     const { value1, value2 } = parseArguments(process.argv);
     multiplicator(value1, value2, `Multiplied ${value1} and ${value2}, the result is:`);
   } catch (error: unknown) {
-    let errorMessage = 'Something bad happened.'
+    let errorMessage = 'Something bad happened.';
     if (error instanceof Error) {
       errorMessage += ' Error: ' + error.message;
     }
